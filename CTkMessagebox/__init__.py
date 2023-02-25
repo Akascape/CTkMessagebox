4a39cd4444e128c6f109c060b886b441ef6c3234
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-02-25 18:36:13 +0530

    Update __init__.py

--- CTkMessagebox/__init__.py
+++ CTkMessagebox/__init__.py
@@ -5,7 +5,7 @@ This is a modern messagebox made with customtkinter.
 Homepage: https://github.com/Akascape/CTkMessagebox
 """
 
-__version__ = '1.0'
+__version__ = '1.1'
 
 from .ctkmessagebox import CTkMessagebox
 
