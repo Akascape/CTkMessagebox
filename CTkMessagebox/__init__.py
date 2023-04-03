7cd8fff50b6aa8e340eee04cbe829094b5784ac9
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-04-03 21:04:30 +0530

    fixed some bugs, added fade-in window effect

--- CTkMessagebox/__init__.py
+++ CTkMessagebox/__init__.py
@@ -5,7 +5,7 @@ This is a modern messagebox made with customtkinter.
 Homepage: https://github.com/Akascape/CTkMessagebox
 """
 
-__version__ = '1.72'
+__version__ = '1.73'
 
 from .ctkmessagebox import CTkMessagebox
 
