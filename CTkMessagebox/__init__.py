f875e61c480027d7fb6698ad1a6c33def759239d
Akash Bora <89206401+Akascape@users.noreply.github.com>
2023-04-14 18:09:18 +0530

    Fixed minor bugs

--- CTkMessagebox/__init__.py
+++ CTkMessagebox/__init__.py
@@ -5,7 +5,7 @@ This is a modern messagebox made with customtkinter.
 Homepage: https://github.com/Akascape/CTkMessagebox
 """
 
-__version__ = '1.74'
+__version__ = '1.8'
 
 from .ctkmessagebox import CTkMessagebox
 
